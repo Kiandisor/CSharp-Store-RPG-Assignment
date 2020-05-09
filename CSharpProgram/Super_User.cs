a718b3dd810a0d6878726e487c65586f918d2282
Kiandisor <59274326+Kiandisor@users.noreply.github.com>
2020-05-09 22:17:23 +1000

    Formating for the file names and added a super user class

---
+++ CSharpProgram/Super_User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store_RPG_Assignment {
+    class Super_User {
+        public void AddItem(ref List<Inventory_Item> PlayerInventor) {        
+            PlayerInventor.Add(new Inventory_Item() {
+            Item_Name = Console.ReadLine(),
+            Item_Amount = ,
+            Item_Cost = ,
+            Item_Pages = 
+            });
+        }
+    }
+}
